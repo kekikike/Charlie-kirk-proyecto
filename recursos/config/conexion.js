7062d3f76e230cf105fc757833f11e98a82faee9
Favionetah <faviosandy30@gmail.com>
2026-01-27 10:45:01 -0400

    ahora dice QR en metodo pago

--- recursos/config/conexion.js
+++ recursos/config/conexion.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', 
-  password: 'cc++4kglt', 
+  password: 'root', 
   database: 'kirkmark'
 });
 
